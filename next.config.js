5786ab7a1fd1c57d125cf462c11dfcfddf30bfc0
Rahul Sheelavantar <rahulsheelavantar@gmail.com>
2023-01-28 10:13:20 +0530

    Added env.local

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
 
 module.exports = {
   nextConfig,
-  env: {
-    secret: "hehe",
-    MONGO: `mongodb+srv://rahulsheelavantar:iamRsheelavantar@cluster0.fjcbof4.mongodb.net/project-ray?retryWrites=true&w=majority`,
-  },
+  // env: {
+  //   secret: "hehe",
+  //   MONGO: `mongodb+srv://rahulsheelavantar:iamRsheelavantar@cluster0.fjcbof4.mongodb.net/project-ray?retryWrites=true&w=majority`,
+  // },
 };
